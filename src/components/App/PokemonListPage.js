cf2d1e09dc684015e975c19eb1e78df90963275f
Yusuf Yalim <yusufyalim@gmail.com>
2020-06-02 00:59:41 +0300

    Add filter mechanism

--- src/components/App/PokemonListPage.js
+++ src/components/App/PokemonListPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import PokemonLink from '../PokemonLink';
 import LoadingIndicator from '../LoadingIndicator';
@@ -6,6 +6,7 @@ import { handleGetPokemonListAction } from '../../store/pokemons';
 import './PokemonListPage.scss';
 
 export default function PokemonListPage() {
+  const [searchTerm, setSearchTerm] = useState('');
   const loading = useSelector((state) => state.loading);
   const pokemons = useSelector((state) => state.pokemons);
   const dispatch = useDispatch();
@@ -14,14 +15,34 @@ export default function PokemonListPage() {
     dispatch(handleGetPokemonListAction());
   }, []);
 
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filterByName = (value) => {
+    return value.includes(searchTerm);
+  };
+
   return (
     <div className="pokemon-list-page">
-      <div className="header" />
+      <div className="pokemon-list-page--header" />
+
+      <div className="pokemon-list-page--search-container">
+        <input
+          className="pokemon-list-page--search"
+          type="search"
+          placeholder="search by name"
+          onChange={handleSearchChange}
+        />
+      </div>
+
       {loading && <LoadingIndicator />}
-      <div className="pokemon-list">
-        {Object.keys(pokemons).map((pokemonName) => (
-          <PokemonLink key={pokemonName} name={pokemonName} />
-        ))}
+      <div className="pokemon-list-page--pokemon-list">
+        {Object.keys(pokemons)
+          .filter(filterByName)
+          .map((pokemonName) => (
+            <PokemonLink key={pokemonName} name={pokemonName} />
+          ))}
       </div>
     </div>
   );
